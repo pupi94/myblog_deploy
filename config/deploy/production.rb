1c4cebb2cd29311ceaad4ed9b01a91128c4f1af8
puping <pingbro@163.com>
2018-05-30 15:56:10 +0800

    添加配置说明

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,7 @@
 server "localhost", user: "ping", roles: %w{app db web}, primary: true
 
+# 将本机的ssh公钥添加到production 服务器的 ~/.ssh/authorized_key 文件中，
+# 然后每次部署的时候就不需要输入很多次密码
 set :ssh_options, {
   keys: %w(~/.ssh/id_rsa),
   forward_agent: true,
